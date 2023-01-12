3e673c09bd0a7a363d915abc6c45fc5879505ccb
Mathew007z <fokker007@hotmail.com>
2023-01-12 15:48:33 -0300

    actualizacion

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ class ProductMananger {
     // Creacion del constructor iniciado con un array vacio de productos.
     constructor(){  
         this.productos = [];
+        // Inicia con array vacio
+        console.log(this.productos)
     }
-
+   
     // getProductos devuelve un array vacio.
     getProductos = () => {
         return this.productos;
- 
+        
     }
 
     addProduct = (title= 'producto a prueba', description='esto es un producto a prueba', price = 200, thumbnail='sin imagen', code='123', stock = 25) => {
@@ -22,6 +24,12 @@ class ProductMananger {
             code,
             stock,
         }
+
+        const values = Object.values(producto)
+        if(values === code.value){
+         console.log('error duplicado')
+         return
+        }
          
         // validacion para agregar id, y requerir todos los campos.
         if (this.productos.length === 0) {
@@ -29,10 +37,23 @@ class ProductMananger {
             producto.id = 1;
         }else{
             producto.id = this.productos[this.productos.length - 1].id +1;
-            console.log('todos los campos son requeridos')
+            
         }
 
-        this.productos.push(producto);
+
+        if(this.productos.includes('code')){
+            console.log('error')
+        }
+            this.productos.push(producto);  
+        
+      
+
+
+        // Console log para ver el producto pusheado
+        console.log(producto)
+
+  
+    
         
     }
 
@@ -41,25 +62,17 @@ class ProductMananger {
 
     // Encontrar producto por ID
     encontrarProducto =  (idProducto) => {
-        const productoIndex = this.productos.findIndex(e => e.id === idProducto);
+       const encontrar = this.productos.find(id => id.id === idProducto.id)
 
-        if(productoIndex === -1){
+        if(!encontrar){
             console.log('producto no encontrado')
-            return
+        }else{
+            console.log(encontrar)
         }
-        
-        const productoDuplicadoCode = this.productos[productoIndex].producto.includes('code')
-
-        if(productoDuplicadoCode){
-            console.log('producto ya encontrado')
-            return
-        }
-
-        this.productos[productoIndex].id.push(idProducto);
-        
     }
 
 
+ 
 }
 
 
@@ -68,5 +81,7 @@ const manejadorProductos =  new ProductMananger();
 
 
 manejadorProductos.addProduct('manzana', 'roja', 200, 'imagen', 123, 5)
-manejadorProductos.encontrarProducto(1,2) 
+manejadorProductos.encontrarProducto(1) 
+manejadorProductos.addProduct('pera', 'verde', 300, 'pera', 222, 2)
+// manejadorProductos.encontrarProducto(1,2) 
 console.log(manejadorProductos.getProductos())
